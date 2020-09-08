94fb1c7f7acad6ea45e4b0aad2170ecf5bae6e1a
Khoa Bui <kbui@zendesk.com>
2020-09-08 08:32:47 +0100

    remove typewriter effect

--- src/components/elements/Quote.js
+++ src/components/elements/Quote.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react"
 import fetch from "node-fetch"
 import style from "./quote.module.css"
-import ReactTypingEffect from 'react-typing-effect'
+import { Row, Col } from '@zendeskgarden/react-grid';
+import { Skeleton } from '@zendeskgarden/react-loaders'
 
 export default function Quote() {
+  const [isLoading, setLoad] = useState(true)
   const [quote, setQuote] = useState(null)
   const [greeter, setGreeter] = useState(null)
 
@@ -14,14 +16,15 @@ export default function Quote() {
 
       setQuote(json.quote)
       setGreeter(json.greeter.split('!')[0] + '!')
+      setLoad(false)
     }
     fetchData()
   }, [])
 
-  return quote && greeter ? (
+  return !isLoading ? (
     <>
     <div className={style.greeting}>
-      <ReactTypingEffect text={greeter}/>
+      {greeter}
     </div>
     <div
       style={{
@@ -39,6 +42,18 @@ export default function Quote() {
     </div>
     </>
   ) : (
-    ""
+  <div style={{
+    padding: '1rem'
+  }}> 
+    <Row justifyContent="center">
+      <Col sm={5}>
+        <Skeleton height="48px" />
+        <Skeleton height="24px" width="80%" />
+        <Skeleton height="16px" width="90%" />
+        <Skeleton height="16px" width="85%" />
+        <Skeleton height="16px" width="75%" />
+      </Col>
+    </Row>
+  </div> 
   )
 }
