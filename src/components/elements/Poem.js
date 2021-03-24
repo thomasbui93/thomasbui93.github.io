dd7ac89b12d89c5a3b7d5e611080d9d33e7d3508
Khoa Bui <kbui@zendesk.com>
2021-03-24 23:15:50 +0000

    Correct randomize logic

--- src/components/elements/Poem.js
+++ src/components/elements/Poem.js
@@ -15,7 +15,7 @@ export default function Poem() {
   const fetchData = async () => {
     setLoad(true)
     const authors = ["L%C3%BD%20B%E1%BA%A1ch", "Nguy%E1%BB%85n%20Tr%C3%A3i"]
-    const author = authors[Math.floor(Math.random(authors.length))]
+    const author = authors[Math.floor(Math.random() * authors.length)]
     const response = await fetch(
       process.env.POEM_URL ||
         `https://griffin-me.herokuapp.com/api/poem/random?author=${author}`
