07cc0a6ff4ba4e8c2b63cf7be82d8b3782503900
thomasbui93 <thomasbui93@users.noreply.github.com>
2021-04-10 14:08:08 +0000

    Apply prettier changes

--- src/components/elements/CareerPiece.js
+++ src/components/elements/CareerPiece.js
@@ -1,51 +1,69 @@
 import { Col, Row } from "@zendeskgarden/react-grid"
-import { Avatar } from "@zendeskgarden/react-avatars";
+import { Avatar } from "@zendeskgarden/react-avatars"
 import { MD, SM, UnorderedList } from "@zendeskgarden/react-typography"
 import React from "react"
 import style from "./career-piece.module.css"
 
-export const CareerPiece = (props) => (<div className={style.container}>
-  <Col justifyContent="center">
-    <Row className={style.headline}>
-      <div className={style.companyIcon}>
-        <Avatar size="large" isSystem>
-          <img src={props.companyIcon} alt={props.companyName}/>
-        </Avatar>
-      </div>
-      <div className={style.companyBox}>
+export const CareerPiece = props => (
+  <div className={style.container}>
+    <Col justifyContent="center">
+      <Row className={style.headline}>
+        <div className={style.companyIcon}>
+          <Avatar size="large" isSystem>
+            <img src={props.companyIcon} alt={props.companyName} />
+          </Avatar>
+        </div>
+        <div className={style.companyBox}>
+          <Col>
+            <MD isBold>{props.companyName}</MD>
+            <SM>
+              {props.start} - {props.end}
+            </SM>
+          </Col>
+          <Col>
+            <Row>
+              {props.skills.map((skill, index) => (
+                <div className={style.skillIcon}>
+                  <Avatar size="extrasmall" key={index}>
+                    <img
+                      src={require(`../../images/skills/${skill.toLowerCase()}.${
+                        isSvg(skill) ? "svg" : "png"
+                      }`)}
+                      alt={skill}
+                    />
+                  </Avatar>
+                </div>
+              ))}
+            </Row>
+          </Col>
+        </div>
+      </Row>
+      <Row>
+        <div className={style.progressTracker}></div>
         <Col>
-          <MD isBold>{props.companyName}</MD>
-          <SM>{props.start} - {props.end}</SM>
+          {props.details.map((detail, index) => (
+            <Row>
+              <JobPiece key={index} {...detail} />
+            </Row>
+          ))}
         </Col>
-        <Col>
-          <Row>
-            {props.skills.map((skill, index) => <div className={style.skillIcon}>
-              <Avatar size="extrasmall" key={index}>
-                <img src={require(`../../images/skills/${skill.toLowerCase()}.${ isSvg(skill) ? "svg": "png"}`)} alt={skill}/>
-              </Avatar>
-            </div>
-            )}
-          </Row>
-        </Col>
-      </div>
-    </Row>
-    <Row>
-      <div className={style.progressTracker}></div>
-      <Col>
-        {props.details.map((detail, index) => <Row>
-          <JobPiece key={index} {...detail}/>
-        </Row>)}
-      </Col>
-    </Row>
+      </Row>
+    </Col>
+  </div>
+)
+
+const isSvg = key => ["spinnaker", "vue.js"].indexOf(key.toLowerCase()) > -1
+
+export const JobPiece = props => (
+  <Col className={style.jobPiece}>
+    <MD isBold>{props.title}</MD>
+    <SM>
+      {props.start} - {props.end}
+    </SM>
+    <UnorderedList>
+      {props.duties.map((duty, index) => (
+        <UnorderedList.Item key={index}>{duty}</UnorderedList.Item>
+      ))}
+    </UnorderedList>
   </Col>
-</div>)
-
-const isSvg = (key) => ["spinnaker", "vue.js"].indexOf(key.toLowerCase()) > -1;
-
-export const JobPiece = (props) => (<Col className={style.jobPiece}>
-  <MD isBold>{props.title}</MD>
-  <SM >{props.start} - {props.end}</SM>
-  <UnorderedList>
-    {props.duties.map((duty, index) => <UnorderedList.Item key={index}>{duty}</UnorderedList.Item>)}
-  </UnorderedList>
-</Col>)
+)
--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,13 +1,17 @@
 import React from "react"
 import Page from "../utils/Page"
-import { Career } from "../components/elements/Career";
+import { Career } from "../components/elements/Career"
 
 export default function CV() {
-  return <Page name="cv">
-    <div style={{
+  return (
+    <Page name="cv">
+      <div
+        style={{
           padding: "1rem 0",
-        }}>
-      <Career />
-    </div>
-  </Page>
+        }}
+      >
+        <Career />
+      </div>
+    </Page>
+  )
 }
