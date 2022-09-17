0ce73637e9b6cd36fd79bdb20da84098d54ec5d3
thomasbui93 <thomasbui93@users.noreply.github.com>
2022-09-17 16:20:22 +0000

    Apply prettier changes

--- src/components/elements/MatchMe.tsx
+++ src/components/elements/MatchMe.tsx
@@ -19,7 +19,6 @@ const shuffle = (array: number[]): number[] => {
   while (currentIndex != 0) {
     randomIndex = Math.floor(Math.random() * currentIndex)
     currentIndex--
-
     ;[array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
